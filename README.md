a1eccb3aef9c0b73e63ac10a3cf15be564b46afd
Jonathan Cammisuli <jon@cammisuli.ca>
2021-12-01 09:56:44 -0500

    setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2021
+
+## Structure
+
+This is setup as a Cargo workspace with one crate for each day's problem, plus any additional. Each crate will be a binary and
+can all be executed simply via `cargo run` or a specific day via `cargo run -p day*`.
---
+++ crates/day1/src/main.rs
@@ -0,0 +1,40 @@
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    println!("input: {:?}", INPUT);
+    // println!("problem1: {}", solve_problem1(INPUT));
+    // println!("problem2: {}", solve_problem2(INPUT));
+}
+
+#[allow(unused)]
+fn solve_problem1(input: &str) -> i32 {
+    todo!()
+}
+
+#[allow(unused)]
+fn solve_problem2(input: &str) -> i32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{solve_problem1, solve_problem2};
+
+    #[test]
+    #[ignore = "day not available"]
+    fn problem1() {
+        let input = "";
+        let expected = 0;
+        let actual = solve_problem1(input);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    #[ignore = "day not available"]
+    fn problem2() {
+        let input = "";
+        let expected = 0;
+        let actual = solve_problem2(input);
+        assert_eq!(expected, actual);
+    }
+}
