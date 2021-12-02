657565b48f0063e10396d9513e9ee0c7b36a334e
Jonathan Cammisuli <jon@cammisuli.ca>
2021-12-02 09:11:39 -0500

    day 2

--- crates/day2/src/main.rs
+++ crates/day2/src/main.rs
@@ -1,21 +1,53 @@
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("input: {:?}", INPUT);
-    // println!("problem1: {}", solve_problem1(INPUT));
-    // println!("problem2: {}", solve_problem2(INPUT));
+    println!("problem1: {}", solve_problem1(INPUT));
+    println!("problem2: {}", solve_problem2(INPUT));
 }
 
 #[allow(unused)]
 fn solve_problem1(input: &str) -> i32 {
-    todo!()
+    let mut depth = 0;
+    let mut forward = 0;
+
+    for line in input.lines() {
+        let mut split = line.split_whitespace();
+        let movement = split.next();
+        let amount = split.next().and_then(|x| x.parse::<i32>().ok());
+        match (movement, amount) {
+            (Some("forward"), Some(count)) => forward += count,
+            (Some("down"), Some(count)) => depth += count,
+            (Some("up"), Some(count)) => depth -= count,
+            _ => (),
+        }
+    }
+
+    depth * forward
 }
 
 #[allow(unused)]
 fn solve_problem2(input: &str) -> i32 {
-    todo!()
-}
+    let mut depth = 0;
+    let mut forward = 0;
+    let mut aim = 0;
 
+    for line in input.lines() {
+        let mut split = line.split_whitespace();
+        let movement = split.next();
+        let amount = split.next().and_then(|x| x.parse::<i32>().ok());
+        match (movement, amount) {
+            (Some("forward"), Some(movement)) => {
+                forward += movement;
+                depth += (movement * aim);
+            }
+            (Some("down"), Some(movement)) => aim += movement,
+            (Some("up"), Some(movement)) => aim -= movement,
+            _ => (),
+        }
+    }
+
+    depth * forward
+}
 
 #[cfg(test)]
 mod test {
