bc12b2d07d2df5ec3be8f8a999792b70a1cf3b52
Jonathan Cammisuli <jon@cammisuli.ca>
2021-12-01 21:31:01 -0500

    day 1

--- crates/day1/src/main.rs
+++ crates/day1/src/main.rs
@@ -1,19 +1,37 @@
+use itertools::Itertools;
+
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("input: {:?}", INPUT);
-    // println!("problem1: {}", solve_problem1(INPUT));
-    // println!("problem2: {}", solve_problem2(INPUT));
+    // println!("input: {:?}", INPUT);
+    println!("problem1: {}", solve_problem1(INPUT));
+    println!("problem2: {}", solve_problem2(INPUT));
 }
 
 #[allow(unused)]
-fn solve_problem1(input: &str) -> i32 {
-    todo!()
+fn solve_problem1(input: &str) -> usize {
+    get_numbers(input)
+        .windows(2)
+        .filter(|window| window[0] < window[1])
+        .count()
 }
 
 #[allow(unused)]
-fn solve_problem2(input: &str) -> i32 {
-    todo!()
+fn solve_problem2(input: &str) -> usize {
+    get_numbers(input)
+        .windows(3)
+        .map(|windows| windows.iter().sum::<i32>())
+        .collect_vec()
+        .windows(2)
+        .filter(|windows| windows[0] < windows[1])
+        .count()
+}
+
+fn get_numbers(input: &str) -> Vec<i32> {
+    input
+        .lines()
+        .map(|i| i.parse::<i32>().unwrap())
+        .collect_vec()
 }
 
 #[cfg(test)]
